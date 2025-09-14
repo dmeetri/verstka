f6fcabc9d7edda950cd808b830d7fb9f3767f07a
dmeetri <145245244+dmeetri@users.noreply.github.com>
2025-09-14 21:44:01 +0900

    Hot Potato

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,44 @@ createRoot(document.getElementById('root')!).render(
         <Header />
 
         <main>
-            <div className="conatainer">
+            <div className="container">
+                <div className="main-content flex">
+                    <div className="main-info">
+                        <div className="main-text-info">
+                            <div className="main-title ">
+                                <h1>
+                                    Decentralized
+                                    <img 
+                                        src="/images/krugla.svg" 
+                                        alt="КРУГЛА"
+                                    />
+                                </h1>
+                                <h1>
+                                    <span className="main-highlight">AI/ML</span> computing
+                                </h1>
+                            </div>
 
+                            <div className="main-subtitle">
+
+                            </div>
+                        </div>
+
+                        <div className="buttons flex">
+                            <button>View Mode</button>
+                            <button>Buy GPU</button>
+                        </div>
+                    </div>
+
+                    <img 
+                        className='main-graphics-right'
+                        src="/images/graphics_right.png"
+                        alt="Микросхема справа"
+                    />
+                </div>
+
+                <div className="main-info flex">
+
+                </div>
             </div>
         </main>
     </StrictMode>
